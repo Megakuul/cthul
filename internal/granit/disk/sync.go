709e2c92fc89af24685bd1c1a3da5073e1537802
Megakuul <linus.moser@megakuul.ch>
2025-04-28 22:58:35 +0200

    feat: update granit api + add different poc for granit disk syncer

--- internal/granit/disk/sync.go
+++ internal/granit/disk/sync.go
@@ -26,78 +26,112 @@ import (
 	"path/filepath"
 	"strings"
 	"sync"
+	"sync/atomic"
 
 	"cthul.io/cthul/pkg/api/granit/v1/disk"
 	"google.golang.org/protobuf/proto"
 )
 
 func (o *Operator) synchronize() {
-  attached := map[string]bool{}
-  attachedLock := sync.RWMutex{}
+	primaryChan := make(chan bool)
+	configChan := make(chan string)
 
-  o.syncer.Add("/GRANIT/DISK/CLUSTERNODES/", o.updateCycleTTL, func(ctx context.Context, k, nodes string) error {
-    id := strings.TrimPrefix(k, "/GRANIT/DISK/CLUSTERNODES/")
-    attachedLock.RLock()
-    defer attachedLock.RUnlock()
-    if attached[id] {
-      return nil
-    }
+	o.operationWg.Add(1)
+	go func() {
+		defer o.operationWg.Done()
+		primary := false
+		config := ""
+		for {
+			select {
+			case <-o.rootCtx.Done():
+				return
+			case primary = <-primaryChan:
+			case config = <-configChan:
+			}
 
-    configKey := fmt.Sprintf("/GRANIT/DISK/CONFIG/%s", id)
-    if nodes[o.nodeId] {
-      o.syncer.Remove(configKey, true)
-      o.syncer.Add(configKey, o.syncCycleTTL, func(ctx context.Context, k, v string) error {
-        // drbdadm up r0 
-        // drbdadm secondary r0 o.nodeId
-      })
-    } else {
-      o.syncer.Remove(configKey, false)
-      // umount /dev/drbdxy
-      // umount /dev/loopdev
-      // rm -rf /device.img
-    }
-  })
-  
-  o.syncer.Add("/GRANIT/DISK/REQNODE/", o.updateCycleTTL, func(ctx context.Context, k, reqnode string) error {
-    id := strings.TrimPrefix(k, "/GRANIT/DISK/REQNODE/")
-    configKey := fmt.Sprintf("/GRANIT/DISK/CONFIG/%s", id)
-    if reqnode == o.nodeId {
-      o.syncer.Remove(configKey, true)
-      o.syncer.Add(configKey, o.syncCycleTTL, func(ctx context.Context, k, v string) error {
-        // drbdadm up r0
-        // drbdadm primary r0 o.nodeid
-        err := o.applyConfig(v)
-        if err!=nil {
-          return err
-        }
-        _, err = o.client.Set(ctx, fmt.Sprintf("/GRANIT/DISK/NODE/%s", id), reqnode, 0)
-        if err!=nil {
-          return err
-        }
+      if config == "" {
+			  o.pruneDevice(ctx, id)
+        continue 
+      }
+
+			if primary {
+        err := o.applyConfig(config, true)
+				if err != nil {
+          // logbliblab
+          continue
+				}
+				_, err = o.client.Set(ctx, fmt.Sprintf("/GRANIT/DISK/NODE/%s", id), o.nodeId, 0)
+				if err != nil {
+          // logbliblab
+          continue
+				}
+			} else {
+        err := o.applyConfig(config, false)
+				if err != nil {
+          // logbliblab
+          continue
+				}
+			}
+		}
+	}()
+
+	o.syncer.Add("/GRANIT/DISK/CLUSTERNODES/", o.updateCycleTTL, func(ctx context.Context, k, rawCluster string) error {
+		id := strings.TrimPrefix(k, "/GRANIT/DISK/CLUSTERNODES/")
+		configKey := fmt.Sprintf("/GRANIT/DISK/CONFIG/%s", id)
+
+		cluster := &disk.DiskCluster{}
+		err := proto.Unmarshal([]byte(rawCluster), cluster)
+		if err != nil {
+			return err
+		}
+		if _, ok := cluster.Nodes[o.nodeId]; ok {
+			o.syncer.Add(configKey, o.syncCycleTTL, func(ctx context.Context, k, v string) error {
+        configChan <- v
         return nil
-      })
-    } else {
-      o.syncer.Remove(configKey, true)
-      // drbdadm secondary r0 o.nodeId
-    }
-    return nil
-  })
+			})
+		} else {
+			o.syncer.Remove(configKey, true)
+      configChan <- ""
+		}
+		return nil
+	})
+
+	o.syncer.Add("/GRANIT/DISK/REQNODE/", o.updateCycleTTL, func(ctx context.Context, k, reqnode string) error {
+    primaryChan <- reqnode == o.nodeId
+		return nil
+	})
 }
 
-func (o *Operator) applyConfig(rawConfig string) error {
-  config := &disk.DiskConfig{}
-  err := proto.Unmarshal([]byte(rawConfig), config)
-  if err!=nil {
-		return fmt.Errorf("failed to parse config: %w", err)
-  }
+func (o *Operator) applyPrimary(rawConfig string) error {
+	// drbdadm up r0
+	// drbdadm primary r0 o.nodeid
+}
 
-  cleanPath := filepath.Join(o.runRoot, config.Path)  
-  if !strings.HasPrefix(cleanPath, o.runRoot) {
-    return fmt.Errorf("device socket path is not allowed to escape the run root ('%s' => '%s')", o.runRoot, cleanPath)
-  }
-  err = os.MkdirAll(filepath.Dir(cleanPath), 0600)
-  if err!=nil {
-    return err
-  }
-  return os.Chmod(filepath.Dir(cleanPath), 0600)
+func (o *Operator) applySecondary(rawConfig string) error {
+	// drbdadm secondary r0 o.nodeId
+}
+
+func (o *Operator) pruneDevice(ctx context.Context, id, node string) {
+
+	// umount /dev/drbdxy
+	// umount /dev/loopdev
+	// rm -rf /device.img
+}
+
+func (o *Operator) applyConfig(rawConfig string, primary bool) error {
+	config := &disk.DiskConfig{}
+	err := proto.Unmarshal([]byte(rawConfig), config)
+	if err != nil {
+		return fmt.Errorf("failed to parse config: %w", err)
+	}
+
+	cleanPath := filepath.Join(o.runRoot, config.Path)
+	if !strings.HasPrefix(cleanPath, o.runRoot) {
+		return fmt.Errorf("device socket path is not allowed to escape the run root ('%s' => '%s')", o.runRoot, cleanPath)
+	}
+	err = os.MkdirAll(filepath.Dir(cleanPath), 0600)
+	if err != nil {
+		return err
+	}
+	return os.Chmod(filepath.Dir(cleanPath), 0600)
 }
